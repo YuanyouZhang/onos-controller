a1d04354fa04b68322897b1dfb89cf5d4b8645f1
zhangyuanyou <zhangyuanyou@huawei.com>
2016-05-25 14:46:19 +0800

    add soft link and add config install-url,profile

---
+++ hooks/onos_controller_utils.py
@@ -0,0 +1,59 @@
+import subprocess
+from os import environ
+import urlparse
+
+from charmhelpers.core.templating import render
+from charmhelpers.core.hookenv import config
+from charmhelpers.core.decorators import retry_on_exception
+
+
+PROFILES = {
+    "openvswitch-onos": {
+        "feature:install": ["onos-app-vtn-onosfw", "onos-openflow-base",
+                            "onos-openflow"],
+        "port": 8080
+    },
+    "openvswitch-onos-goldeneye": {
+        "feature:install": ["onos-ovsdatabase",
+                            "onos-ovsdb-base",
+                            "onos-drivers-ovsdb",
+                            "onos-ovsdb-provider-host",
+                            "onos-app-vtn-onosfw",
+                            "onos-openflow-base",
+                            "onos-openflow"],
+        "port": 8080
+    },
+}
+PROFILES["default"] = PROFILES["openvswitch-onos"]
+
+
+@retry_on_exception(5, base_delay=10, exc_type=subprocess.CalledProcessError)
+def run_onos(cmds):
+    run_cmd = ["/opt/onos/bin/onos"]
+    run_cmd.extend(cmds)
+    output = subprocess.check_output(run_cmd)
+    return output
+
+
+def installed_features():
+    installed = []
+    out = run_onos(["feature:list"])
+    for line in out.split("\n"):
+        columns = line.split("|")
+        if len(columns) > 2:
+            install_flag = columns[2].replace(" ", "")
+            if install_flag == "x":
+                installed.append(columns[0].replace(" ", ""))
+    return installed
+
+
+def filter_installed(features):
+    installed = installed_features()
+    whitelist = [feature for feature in features if feature not in installed]
+    return whitelist
+
+
+def process_onos_cmds(onos_cmds):
+    features = filter_installed(onos_cmds.get("feature:install", []))
+    if features:
+        run_onos(["feature:install"] + features)
--- hooks/onos_hook.py
+++ hooks/onos_hook.py
@@ -17,42 +17,51 @@ from charmhelpers.core.host import (
     service_start,
     service_stop
 )
-
+from onos_controller_utils import process_onos_cmds
+from onos_controller_utils import PROFILES
 from subprocess import check_call
 
 hooks = Hooks()
-config = config()
+#config = config()
 
 @hooks.hook("config-changed")
 def config_changed():
-    pass 
+    gatewaymac=config("gateway-mac")
+    if (gatewaymac is not None) and len(gatewaymac) > 4 :
+        if gatewaymac != "default" :
+            print 'gatewaymac actually value is ' + gatewaymac
+            check_call("/opt/onos/bin/onos \'externalgateway-update -m " + gatewaymac + "\'",shell=True) 
 
 @hooks.hook("controller-api-relation-joined")
 def controller_api_joined():
     relation_set(port=8181, username="admin", password="admin")
 
-@hooks.hook()
+@hooks.hook("install")
 def install():
         #install jdk and onos
         shutil.copy("files/onos_install.sh", "/opt")
         shutil.copy("files/onos.conf", "/etc/init")
-        check_call("sh /opt/onos_install.sh",shell=True)
+        if config("install-url"):
+            url=config("install-url")
+            check_call("sh /opt/onos_install.sh "+url,shell=True)
 
 
-@hooks.hook()
+@hooks.hook("start")
 def start():
         service_start("onos")
         check_call("sleep 60", shell=True)
-        check_call(["/opt/onos/bin/onos", "-r", "61",
-                "feature:install", "onos-app-vtn-onosfw", "onos-openflow-base","onos-openflow"])
-        check_call("/opt/onos/bin/onos \"externalportname-set -n eth1\"", shell=True)
+        if config("profile"):
+            process_onos_cmds(PROFILES[config("profile")])
+        extport = config("ext-port")
+        print 'external port is ' + extport
+        check_call("/opt/onos/bin/onos \"externalportname-set -n "+extport+"\"", shell=True)
 
 @hooks.hook("ovsdb-manager-relation-joined")
 def ovsdb_manager_joined():
     relation_set(port=6640, protocol="tcp")
 
 
-@hooks.hook()
+@hooks.hook("stop")
 def stop():
         service_stop("onos")
 
